0350cda77548b86c437412fd3efbfbfbbd911281
ahmadali shafiee <mail@ahmadalli.net>
2018-02-08 18:33:49 +0330

    Gene ToString compatible with client requirement

--- Colosseum.GS/Gene.cs
+++ Colosseum.GS/Gene.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace Colosseum.GS
 {
@@ -7,5 +8,10 @@ namespace Colosseum.GS
         public const int LengthOfGene = 10;
         public List<double> GenomesList { get; private set; } = new List<double>(LengthOfGene);
         public double Score { get; set; } = -1;
+
+        public override string ToString()
+        {
+            return string.Join(Environment.NewLine, GenomesList.Count, string.Join(Environment.NewLine, GenomesList.ToArray()));
+        }
     }
 }
