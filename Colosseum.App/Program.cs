5afb17391a44746f71fcadc038e3ba7b7c48323c
ahmadali shafiee <mail@ahmadalli.net>
2018-02-08 15:48:08 +0330

    Add project files.

---
+++ Colosseum.App/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Colosseum.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
