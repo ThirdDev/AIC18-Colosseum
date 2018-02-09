94c3a9a46debf740e0ec2312f48dd379360b4f8f
ahmadali shafiee <mail@ahmadalli.net>
2018-02-09 05:40:08 +0330

    generate 100 genes per generation

--- Colosseum.GS/GenerationGenerator.cs
+++ Colosseum.GS/GenerationGenerator.cs
@@ -8,7 +8,7 @@ namespace Colosseum.GS
     {
         private const double PartToLive = 0.5;
         private const double rangeOfMutaion = 0.05;
-        public const int generationPopulation = 4;
+        public const int generationPopulation = 100;
         
         
         public List<Gene> randomGeneration ()
