0143b1361962eab6462cb7179b3ff3332991076c
ahmadali shafiee <mail@ahmadalli.net>
2018-02-09 13:32:09 +0330

    set UI Port in server configs

--- Colosseum.App/Server/ServerConfig.cs
+++ Colosseum.App/Server/ServerConfig.cs
@@ -14,7 +14,7 @@ namespace Colosseum.App.Server
         public string ClientsConnectionTimeout { get; private set; } = "2147483647";
         public string UIEnable { get; private set; } = "false";
         public string UIToken { get; private set; } = "00000000000000000000000000000000";
-        public string UIPort { get; private set; } = "7000";
+        public string UIPort { get; set; }
         public string UIConnectionTimeout { get; private set; } = "2147483647";
         public string OCSendToUI { get; private set; } = "false";
         public string OCSendToFile { get; private set; } = "false";
