4961bde353bbd648dc77de9cdf1b9ee7f528b01c
ahmadali shafiee <mail@ahmadalli.net>
2018-02-09 16:56:33 +0330

    format code and sort and clean usings

--- Colosseum.App/Client/ClientManager.cs
+++ Colosseum.App/Client/ClientManager.cs
@@ -1,11 +1,8 @@
 ﻿using Colosseum.App.Server;
 using Colosseum.GS;
 using Colosseum.Services;
-using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
