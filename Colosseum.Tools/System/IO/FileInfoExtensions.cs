4709da770d001a333dc920a6f5f729f7b5a449f5
ahmadali shafiee <mail@ahmadalli.net>
2018-02-13 23:41:32 +0330

    delete file async

---
+++ Colosseum.Tools/System/IO/FileInfoExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System.IO
+{
+    public static class FileInfoExtensions
+    {
+        public static Task DeleteAsync(this FileInfo fi)
+        {
+            return Task.Factory.StartNew(() => fi.Delete());
+        }
+    }
+}
