2b4e27f6b11df463b23e36b8d711a42b84b5be8e
ahmadali shafiee <mail@ahmadalli.net>
2018-02-14 17:40:08 +0330

    exit if one of the processes exited

--- Colosseum.App/run.sh
+++ Colosseum.App/run.sh
@@ -1,4 +1,7 @@
 cd files
-java -Djava.awt.headless=true -jar ../AIC18-Server.jar --config="server.cfg" > server.out &
-java -jar ../Client.jar 127.0.0.1 7099 00000000000000000000000000000000 1000 attack client.attack.cfg > client.attack.out &
-java -jar ../Client.jar 127.0.0.1 7099 00000000000000000000000000000000 1000 defend client.defend.cfg > client.defend.out
+(java -Djava.awt.headless=true -jar ../AIC18-Server.jar --config="server.cfg" > server.out
+exit) &
+(java -jar ../Client.jar 127.0.0.1 7099 00000000000000000000000000000000 1000 attack client.attack.cfg > client.attack.out
+exit) &
+(java -jar ../Client.jar 127.0.0.1 7099 00000000000000000000000000000000 1000 defend client.defend.cfg > client.defend.out
+exit)
