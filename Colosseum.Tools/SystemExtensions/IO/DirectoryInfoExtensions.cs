578f12bd7be6f84ad273f5aafca6df5a95ea4bdf
ahmadali shafiee <mail@ahmadalli.net>
2018-02-15 15:48:11 +0330

    Force Delete Directory

---
+++ Colosseum.Tools/SystemExtensions/IO/DirectoryInfoExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+
+namespace Colosseum.Tools.SystemExtensions.IO
+{
+    public static class DirectoryInfoExtensions
+    {
+        public static void DeleteForce(this DirectoryInfo directoryInfo)
+        {
+            foreach (var file in directoryInfo.GetFiles())
+            {
+                file.Delete();
+            }
+
+            foreach (var dir in directoryInfo.GetDirectories())
+            {
+                dir.DeleteForce();
+            }
+
+            directoryInfo.Delete();
+        }
+    }
+}
