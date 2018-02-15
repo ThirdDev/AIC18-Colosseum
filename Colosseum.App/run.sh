f5102d1f6ee34f9248e633c03924a064df3f0eda
ahmadali shafiee <mail@ahmadalli.net>
2018-02-15 12:52:08 +0330

    docker process output logging and exiting fix

--- Colosseum.App/run.sh
+++ Colosseum.App/run.sh
@@ -1,7 +1,12 @@
+#!/bin/bash
+
 cd files
-(java -Djava.awt.headless=true -jar ../AIC18-Server.jar --config="server.cfg" > server.out
-exit) &
-(java -jar ../Client.jar 127.0.0.1 7099 00000000000000000000000000000000 1000 attack client.attack.cfg > client.attack.out
-exit) &
-(java -jar ../Client.jar 127.0.0.1 7099 00000000000000000000000000000000 1000 defend client.defend.cfg > client.defend.out
-exit)
+(java -Djava.awt.headless=true -jar ../AIC18-Server.jar --config=server.cfg &> server.out;
+echo server exited;
+kill $(ps aux | grep 'java' | awk '{print $2}');) &
+(java -jar ../Client.jar 127.0.0.1 7099 00000000000000000000000000000000 1000 attack client.attack.cfg &> client.attack.out;
+echo attack client exited;
+kill $(ps aux | grep 'java' | awk '{print $2}');) &
+(java -jar ../Client.jar 127.0.0.1 7099 00000000000000000000000000000000 1000 defend client.defend.cfg &> client.defend.out;
+echo defend client exited;
+kill $(ps aux | grep 'java' | awk '{print $2}');)
