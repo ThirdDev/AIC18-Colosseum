412123f621fe0d859f65b85cd3d84b43317de397
Mahdi <mghiasi75@gmail.com>
2018-02-18 01:04:07 +0330

    Fixed an issue causing some games to not start at all

--- Colosseum.Experiment/GeneParsers/MyGeneParser.cs
+++ Colosseum.Experiment/GeneParsers/MyGeneParser.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using Colosseum.GS;
 
@@ -9,12 +10,42 @@ namespace Colosseum.Experiment.GeneParsers
     {
         public Gene Gene { get; private set; }
 
+        int len;
+
         public MyGeneParser(Gene gene)
         {
             Gene = gene;
-        }
 
-        const int len = 15;
+            len = gene.GenomesList.Count / 2;
+
+            var creepGenePart = gene.GenomesList.GetRange(0, len).ToList();
+            var heroGenePart = gene.GenomesList.GetRange(0, len).ToList();
+
+            int counter = 0;
+            for (int i = 0; i < len; i++)
+            {
+                if (counter > len)
+                    break;
+                counter++;
+
+                if (GetSoldierCount(creepGenePart[i]) == 0 && GetSoldierCount(heroGenePart[i]) == 0)
+                {
+                    double g1 = creepGenePart[i];
+                    double g2 = heroGenePart[i];
+
+                    creepGenePart.RemoveAt(i);
+                    heroGenePart.RemoveAt(i);
+                    i--;
+
+                    creepGenePart.Add(g1);
+                    heroGenePart.Add(g2);
+                }
+            }
+
+            gene.GenomesList.Clear();
+            gene.GenomesList.AddRange(creepGenePart);
+            gene.GenomesList.AddRange(heroGenePart);
+        }
 
         public AttackAction Parse(int turn)
         {
@@ -30,9 +61,14 @@ namespace Colosseum.Experiment.GeneParsers
 
             return new AttackAction
             {
-                CountOfCreeps = (int)Math.Max(7, a) - 7,
-                CountOfHeros = (int)Math.Max(7, b) - 7,
+                CountOfCreeps = GetSoldierCount(a),
+                CountOfHeros = GetSoldierCount(b),
             };
+        }
+
+        private static int GetSoldierCount(double a)
+        {
+            return (int)Math.Max(7, a) - 7;
         }
     }
 }
