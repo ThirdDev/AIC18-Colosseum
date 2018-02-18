464f1cc5fd68a3afd6957703a6f62ffdcb8e311a
ahmadali shafiee <mail@ahmadalli.net>
2018-02-18 13:34:41 +0330

    use gaussian random in tower generation

--- Colosseum.Experiment/Program.cs
+++ Colosseum.Experiment/Program.cs
@@ -46,7 +46,7 @@ namespace Colosseum.Experiment
             var maximumCountOfGenerations = 100;
 
             var towerCount = 10;
-            var archerTowerCount = _random.Next(towerCount);
+            var archerTowerCount = (int)gaussianRandom(towerCount, towerCount);
             var canonTowerCount = towerCount - archerTowerCount;
 
             cannons = randomTowerOrder(canonTowerCount, length);
@@ -161,6 +161,18 @@ namespace Colosseum.Experiment
             }
 
             return towers.ToArray();
+        }
+
+        private static double gaussianRandom(double mean, double standarddeviation)
+        {
+            var u1 = 1.0 - _random.NextDouble(); //uniform(0,1] random doubles
+            var u2 = 1.0 - _random.NextDouble();
+            var randStdNormal = Math.Sqrt(-2.0 * Math.Log(u1)) *
+                                   Math.Sin(2.0 * Math.PI * u2); //random normal(0,1)
+            var randNormal =
+                mean + standarddeviation * randStdNormal; //random normal(mean,stdDev^2)
+
+            return randNormal;
         }
     }
 }
