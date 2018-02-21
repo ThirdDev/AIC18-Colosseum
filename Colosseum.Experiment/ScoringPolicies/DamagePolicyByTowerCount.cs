5c9b26192382711fd92e8c4cabd06a20eb735556
ahmadali shafiee <mail@ahmadalli.net>
2018-02-21 04:05:00 +0330

    DamagePolicyByTowerCount for random tower counts

---
+++ Colosseum.Experiment/ScoringPolicies/DamagePolicyByTowerCount.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Colosseum.Experiment.ScoringPolicies
+{
+    internal class DamagePolicyByTowerCount : IScoringPolicy
+    {
+        public double CalculateTotalScore(SimulationResult result)
+        {
+            var _preferredMoneyToSpend = (result.ArcherTowersCount + result.CannonTowersCount) * 100;
+
+            return (result.DamagesToEnemyBase > 0 ? (Math.Min(result.DamagesToEnemyBase, 50)) : 0) * 30.0 +
+                   -Math.Pow(result.TotalPrice / (_preferredMoneyToSpend / 3.0), 4);
+        }
+
+        public int GetPreferredMoneyToSpend()
+        {
+            return -1;
+        }
+    }
+}
